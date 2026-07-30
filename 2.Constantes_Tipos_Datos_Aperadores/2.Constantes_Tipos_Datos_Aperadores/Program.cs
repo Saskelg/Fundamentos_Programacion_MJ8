6dc370e39db820a5bfa39630412fa12683cc1e7c
Saskelg <samuel.burbano@upb.edu.co>
2026-07-30 09:35:48 -0500

    2.constantes y tipos de datos y operadores

---
+++ 2.Constantes_Tipos_Datos_Aperadores/2.Constantes_Tipos_Datos_Aperadores/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _2.Constantes_Tipos_Datos_Aperadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //codigo fuente aca
+            //constantes
+
+            const string iva = "19%";
+            string nombre = "Samuel";
+
+            //cambio de valor en variable, no se puede hacer con las constantes
+
+            nombre = "Pepito Perez";
+
+            //tipos de datos
+
+            //byte: numerico desde 0 a 255
+
+            byte dato1 = 1;
+
+            //int o integer: numerico con mayor limite incluyendo negativos
+
+            int dato2 = 2763;
+
+            //long: numerico que tambien puede contener caracteres
+
+            long dato3 = 1L;
+
+            //float: numerico decimal, se debe incluir f al final o el sistema lo confundira con un double
+
+            float dato4 = 1.0f;
+
+            //double: numerico decimal de dos cifras, se debe poner d al final
+
+            double dato5 = 1.45d;
+
+            //decimal: numerico decimal de cifras mas largas para mas precision, se debe poner m al final
+
+            decimal dato6 = 2.763m;
+
+            //char: variable de UN solo caracter (estrictamente 1) y se deben poner entre comillas singulares ''
+
+            char dato7 = 'A';
+
+            //string: variable de cadena de caracteres, se puede poner cualquier cosa, se deben poner entre comillas dobles
+
+            string dato8 = "OIADIBDAOIDBADOAWDNONO98219Y1283U12B1'1+´´+'| 022'1 ";
+
+            //boolean: dato que solo se puede poner "true" o "false"
+
+            Boolean dato9 = true;
+
+            //object: variable que crea objetos a travez de una clase, todas las clases que se hagan heredan de object
+
+            object dato10 = new object();
+
+            //OPERADORES
+
+            //numericos
+
+            //cambio de signo
+
+            int dato11 = 5;
+            int dato12 = -dato11;
+            Console.WriteLine("dato11: {0}, dato12:{1} ",dato11,dato12);
+
+            //aritmeticos
+
+            int dato13 =  dato11 - dato12;
+            Console.WriteLine("El resultado de la resta es: {0}",dato13);
+            int dato14 = 5 * dato13;
+            Console.WriteLine("El resultado de la multiplicacion es: {0}",dato14);
+            float dato15 = 6f / 7f;
+            Console.WriteLine("El resultado de la division es: {0}", dato15);
+        }
+    }
+}
---
+++ 2.Constantes_Tipos_Datos_Aperadores/2.Constantes_Tipos_Datos_Aperadores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("2.Constantes_Tipos_Datos_Aperadores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2.Constantes_Tipos_Datos_Aperadores")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb3afe68-d92e-42b1-8974-045ae0dd7df0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
