71172f7dc52d792be69877fca80555fb05ab9cfe
Saskelg <samuel.burbano@upb.edu.co>
2026-09-01 09:28:47 -0500

    Cilos while y do while
    si ultimo ejercicio no funciona e snormal, lo actualizare despues

---
+++ 11.2.Ciclos_Do_While/11.2.Ciclos_Do_While/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace _11._Ciclos_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+
+            /*int Contador = 0;
+
+            int Acumulador = 0;
+
+            //ciclo while
+
+            do
+            {
+                Contador++;
+                Acumulador = Acumulador + Contador;
+            }
+            while (Contador < 5);
+
+            //resultado
+
+            Console.WriteLine("La suma es: " +  Acumulador);*/
+
+            //variables
+
+            /*int numero = 0;
+
+            int acumulador = 1;
+
+            int limite = 11;
+
+            //interfaz usuario
+
+            Console.WriteLine("Que tabla de numero quiere ver?");
+
+            numero = int.Parse(Console.ReadLine());
+
+            //ciclo
+
+            do
+            {
+                Console.WriteLine(numero + " x " + acumulador + " = " + numero * acumulador);
+
+                acumulador++;
+
+                if (acumulador == limite)
+                {
+                    string pregunta = "";
+                    Console.WriteLine("Desea continuar con la tabla? (si/no)");
+                    pregunta = Console.ReadLine();
+                    if (pregunta == "si")
+                    {
+                        limite = limite + 10;
+                    }
+                }
+            }
+            while (acumulador < limite);*/
+            int numero = 1;
+
+            do
+            {
+                bool primo = true;
+                int divisor = 2;
+
+                if (numero < 2)                {
+                    primo = false;
+                }
+                else
+                {
+                    do
+                    {
+                        if (numero / divisor == 0)
+                        {
+                            primo = false;
+                            break;
+                        }
+                    }
+                    while (divisor * divisor <= numero);
+                }
+                if (primo == true)
+                {
+                    Console.WriteLine(numero);
+                }
+                numero++;
+            }
+            while (numero <= 100);
+        }
+    }
+}
---
+++ 9.Ciclos_While_eEercicios/9.Ciclos_While_eEercicios/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _9.Ciclos_While_eEercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+
+            byte valor_a_introducir = 0;
+
+            int numero = 0;
+
+            byte contador_principal = 0; 
+
+            byte contador_mayor_cero = 0;
+
+            byte contador_menor_cero = 0;
+
+            byte contador_cero = 0;
+
+            //interfaz y entrada
+
+            Console.WriteLine("Ingrese la cantidad de numeros que va a introducir");
+
+            valor_a_introducir = byte.Parse(Console.ReadLine());
+
+            //ciclo
+
+            while (contador_principal != valor_a_introducir) 
+            {
+                Console.WriteLine("Ingrese al valor del numero:");
+
+                numero = int.Parse(Console.ReadLine());
+
+                contador_principal++;
+
+                if (numero == 0)
+                {
+                    contador_cero++;
+                }
+                if (numero > 0)
+                {
+                    contador_mayor_cero++;
+                }
+                if (numero < 0)
+                {
+                    contador_menor_cero++;
+                }
+            }
+
+            //salida
+
+            Console.WriteLine("Cantidad de numeros ingresador mayores que cero: " + contador_mayor_cero);
+
+            Console.WriteLine("Cantidad de numeros ingresador menores que cero: " + contador_menor_cero);
+
+            Console.WriteLine("Cantidad de numeros ingresador iguales a cero: " + contador_cero);
+        }
+    }
+}
