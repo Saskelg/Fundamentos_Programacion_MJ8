d72c1caf367cd725462d0ad78b5da786f6789a38
Saskelg <samuel.burbano@upb.edu.co>
2026-09-03 08:55:37 -0500

    12.Ejercicio_Do_While.

---
+++ 12.Do_while_ejercicio/12.Do_while_ejercicio/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Do_while_ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+
+            int cantidad_de_usuarios = 0;
+
+            int numero_de_cuenta = 0;
+
+            int contador = 0;
+
+            int acumulador_saldos = 0;
+
+            string nombre = "";
+
+            int saldo = 0;
+
+            //interfaz de usuarioo en insertado de datos
+
+            Console.WriteLine("Cuantos Usuarios van a ingresar?");
+
+            cantidad_de_usuarios = int.Parse(Console.ReadLine());
+
+            do
+            {
+                contador++;
+
+                Console.WriteLine("Ingrese su nombre de usuario");
+                nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese su numero de cuenta");
+                numero_de_cuenta = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese su saldo");
+                saldo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("----------Datos de cuenta----------");
+                Console.WriteLine("nombre: " + nombre);
+                Console.WriteLine("numero de cuenta: " + numero_de_cuenta);
+                Console.WriteLine("saldo: " + saldo);
+                Console.WriteLine("------------------------------------");
+
+                if (saldo > 3000000)
+                {
+                    Console.WriteLine("Es apto para el prestamo");
+                }
+                else
+                {
+                    Console.WriteLine("No es apto para el prestamo");
+                }
+
+                acumulador_saldos += saldo;
+
+            }
+            while (contador < cantidad_de_usuarios);
+
+            Console.WriteLine("La cantidad de usuarios es: " + cantidad_de_usuarios);
+
+            Console.WriteLine("El promedio de saldos es: " + (acumulador_saldos / cantidad_de_usuarios));
+
+        }
+    }
+}
---
+++ 12.Do_while_ejercicio/12.Do_while_ejercicio/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("12.Do_while_ejercicio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12.Do_while_ejercicio")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f904cc51-97f6-4ec7-a7fa-3e049f16ef17")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
