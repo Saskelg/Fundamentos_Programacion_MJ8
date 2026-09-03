cd67a2d2726a92652cb1f252d2278cef543acace
Saskelg <samuel.burbano@upb.edu.co>
2026-09-03 09:25:42 -0500

    Add files via upload

---
+++ 13.Ciclos_For/13.Ciclos_For/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _13.Ciclos_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int acumulador = 0;
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine("La suma de los números del 1 al 5 es: " + acumulador);
+        }
+    }
+}
